ff8fa7d255161b69f39e714a007a1b57f9e6c4d1
Victor <garvalov@nerds09.systopia>
2023-12-08 05:56:42 -0800

    fixed config

--- models/transformer/config.py
+++ models/transformer/config.py
@@ -1,5 +1,7 @@
 from pathlib import Path
-from globals import *
+
+
+DATASET_PATH="/home/garvalov/ml_prefetching_project_2/data/canneal_v1.csv" #change depending on which machine is running train.py
 
 def get_config():
     return {
