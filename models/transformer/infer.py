456b6200dc4dc9965e59546488cd8cc1d7976c8e
Thibault Czarniak <thibault.czarniak@epfl.ch>
2023-12-12 17:31:39 +0100

    Inferrrrrrrr

--- models/transformer/infer.py
+++ models/transformer/infer.py
@@ -4,27 +4,30 @@ from pathlib import Path
 import torch
 import torchmetrics
 
-from models.transformer.config import get_device
-from models.transformer.dataset import causal_mask
+from models.transformer.config import get_device, get_config
+from models.transformer.dataset import causal_mask, BilingualDataset
 from tokenizers import Tokenizer
 
+from models.transformer.train import get_model
+
+
 class Inferer:
-    def __init__(self, config, model):
-        tokenizer_src_path = Path(config['tokenizer_src_file'])
-        tokenizer_tgt_path = Path(config['tokenizer_tgt_file'])
+    def __init__(self, config, model=None, device=None):
+        tokenizer_src_path = Path(config['tokenizer_file'].format(config['lang_src']))
+        tokenizer_tgt_path = Path(config['tokenizer_file'].format(config['lang_tgt']))
+
         tokenizer_src = Tokenizer.from_file(str(tokenizer_src_path))
         tokenizer_tgt = Tokenizer.from_file(str(tokenizer_tgt_path))
         self.tokenizer_src = tokenizer_src
         self.tokenizer_tgt = tokenizer_tgt
-
         self.src_lang = config['lang_src']
         self.tgt_lang = config['lang_tgt']
-
-        self.sos_token = torch.tensor([tokenizer_tgt.token_to_id("[SOS]")], dtype=torch.int64)
-        self.eos_token = torch.tensor([tokenizer_tgt.token_to_id("[EOS]")], dtype=torch.int64)
-        self.pad_token = torch.tensor([tokenizer_tgt.token_to_id("[PAD]")], dtype=torch.int64)
+        self.config = config
+        self.device = get_device() if device is None else device
+        self.model = self.load_model("canneal_trace_v1_weights/tmodel_03.pt") if model is None else model #todo modify for portability
 
     def greedy_decode(self, model, source, source_mask, tokenizer_tgt, max_len, device):
+        #todo refactor to use class variables
         sos_idx = tokenizer_tgt.token_to_id('[SOS]')
         eos_idx = tokenizer_tgt.token_to_id('[EOS]')
 
@@ -56,13 +59,12 @@ class Inferer:
         return decoder_input.squeeze(0)
 
 
-    def infer(self, model, data, max_len, device=None, output_file=None):
-        if device is None:
-            device = get_device()
-
-        model.eval()
+    def infer(self, data, max_len, output_file=None):
+        self.model.eval()
         source_texts = []
         predicted = []
+        dataAdapted = {}
+        dataAdapted[0] = {self.src_lang: data, self.tgt_lang: ""}
 
         try:
             # get the console window width
@@ -75,32 +77,27 @@ class Inferer:
 
         with torch.no_grad():
 
-            # Transform the text into tokens
-            enc_input_tokens = self.tokenizer_src.encode(data).ids
+            dataset = BilingualDataset(dataAdapted, self.tokenizer_src, self.tokenizer_tgt, self.src_lang, self.tgt_lang,
+                                       max_len)
 
-            # Add sos, eos and padding to each sentence
-            enc_num_padding_tokens = self.seq_len - len(enc_input_tokens) - 2  # We will add <s> and </s>
-            #todo do we pass only one sentence in inference? or multiple? bc if one sentence computation is not correct
+            #assert(dataset.__len__() == 1)
 
-            # Make sure the number of padding tokens is not negative. If it is, the sentence is too long
-            if enc_num_padding_tokens < 0:
-                raise ValueError("Sentence is too long")
+            dp = dataset.__getitem__(0)
+            encoder_input = dp["encoder_input"].to(self.device)  # (b, seq_len)
+            encoder_mask = dp["encoder_mask"].to(self.device)  # (b, 1, 1, seq_len)
 
-            # Add <s> and </s> token
-            encoder_input = torch.cat(
-                [
-                    self.sos_token,
-                    torch.tensor(enc_input_tokens, dtype=torch.int64),
-                    self.eos_token,
-                    torch.tensor([self.pad_token] * enc_num_padding_tokens, dtype=torch.int64),
-                ],
-                dim=0,
-            )
 
-            encoder_input = encoder_input # (b, seq_len)
-            encoder_mask = (encoder_input != self.pad_token).unsqueeze(0).unsqueeze(0).int()  # (b, 1, 1, seq_len)
+            model_out = self.greedy_decode(self.model, encoder_input, encoder_mask, self.tokenizer_tgt, max_len, self.device)
 
-            model_out = self.greedy_decode(model, encoder_input, encoder_mask, max_len, device)
+            source_text = dp["src_text"][0]
+            # target_text = dp["tgt_text"][0]
+            model_out_text = self.tokenizer_tgt.decode(model_out.detach().cpu().numpy())
+
+            source_texts.append(source_text)
+            # expected.append(target_text)
+            predicted.append(model_out_text)
+
+            model_out = self.greedy_decode(self.model, encoder_input, encoder_mask, self.tokenizer_tgt, max_len, self.device)
 
             model_out_text = self.tokenizer_tgt.decode(model_out.detach().cpu().numpy())
 
@@ -115,14 +112,15 @@ class Inferer:
             if output_file is not None:
                 output_file.write(f"{model_out_text}\n")
 
+    def load_model(self, path):
+        self.model = get_model(self.config, self.tokenizer_src.get_vocab_size(), self.tokenizer_tgt.get_vocab_size()).to(
+            self.device)
+        self.model.load_state_dict(torch.load(path)['model_state_dict'])
+        return self.model
 
-def load_model(path, model=None):
-    if model is None:
-        model = torch.load(path)
-    else:
-        model.load_state_dict(torch.load(path))
-    return model
+
 
 
 if __name__ == '__main__':
-    model = load_model("models/transformer/transformer.pt")
+    inferer = Inferer(get_config())
+    inferer.infer("0x55bb3580f018", 15)
