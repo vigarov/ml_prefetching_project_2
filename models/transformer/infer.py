89f343fc3a0a1cdf6efd020842a3c3cb097205e6
Thibault Czarniak <thibault.czarniak@epfl.ch>
2023-12-13 13:30:00 +0100

    Refactored

--- models/transformer/infer.py
+++ models/transformer/infer.py
@@ -4,7 +4,7 @@ from pathlib import Path
 import torch
 import torchmetrics
 
-from models.transformer.config import get_device, get_config
+from models.transformer.config import get_device, get_config, latest_weights_file_path
 from models.transformer.dataset import causal_mask, BilingualDataset
 from tokenizers import Tokenizer
 
@@ -24,24 +24,24 @@ class Inferer:
         self.tgt_lang = config['lang_tgt']
         self.config = config
         self.device = get_device() if device is None else device
-        self.model = self.load_model("canneal_trace_v1_weights/tmodel_03.pt") if model is None else model #todo modify for portability
+        path_weights = latest_weights_file_path(config)
+        self.model = self.load_model(path_weights) if model is None else model
 
-    def greedy_decode(self, model, source, source_mask, tokenizer_tgt, max_len, device):
-        #todo refactor to use class variables
-        sos_idx = tokenizer_tgt.token_to_id('[SOS]')
-        eos_idx = tokenizer_tgt.token_to_id('[EOS]')
+    def greedy_decode(self, model, source, source_mask, max_len):
+        sos_idx = self.tokenizer_tgt.token_to_id('[SOS]')
+        eos_idx = self.tokenizer_tgt.token_to_id('[EOS]')
 
         # Precompute the encoder output and reuse it for every step
         encoder_output = model.encode(source, source_mask)
         # Initialize the decoder input with the sos token
-        decoder_input = torch.empty(1, 1).fill_(sos_idx).type_as(source).to(device)
+        decoder_input = torch.empty(1, 1).fill_(sos_idx).type_as(source).to(self.device)
         while True:
             if decoder_input.size(1) == max_len:
                 break
 
             # build mask for target
             decoder_mask = causal_mask(decoder_input.size(1)).type_as(source_mask).to(
-                device)  # TODO check if we have to adapt for other datasets
+                self.device)
 
             # calculate output
             out = model.decode(encoder_output, source_mask, decoder_input, decoder_mask)
@@ -50,14 +50,13 @@ class Inferer:
             prob = model.project(out[:, -1])
             _, next_word = torch.max(prob, dim=1)
             decoder_input = torch.cat(
-                [decoder_input, torch.empty(1, 1).type_as(source).fill_(next_word.item()).to(device)], dim=1
+                [decoder_input, torch.empty(1, 1).type_as(source).fill_(next_word.item()).to(self.device)], dim=1
             )
 
             if next_word == eos_idx:
                 break
 
         return decoder_input.squeeze(0)
-
 
     def infer(self, data, max_len, output_file=None):
         self.model.eval()
@@ -77,17 +76,17 @@ class Inferer:
 
         with torch.no_grad():
 
-            dataset = BilingualDataset(dataAdapted, self.tokenizer_src, self.tokenizer_tgt, self.src_lang, self.tgt_lang,
+            dataset = BilingualDataset(dataAdapted, self.tokenizer_src, self.tokenizer_tgt, self.src_lang,
+                                       self.tgt_lang,
                                        max_len)
 
-            #assert(dataset.__len__() == 1)
+            # assert(dataset.__len__() == 1)
 
             dp = dataset.__getitem__(0)
             encoder_input = dp["encoder_input"].to(self.device)  # (b, seq_len)
             encoder_mask = dp["encoder_mask"].to(self.device)  # (b, 1, 1, seq_len)
 
-
-            model_out = self.greedy_decode(self.model, encoder_input, encoder_mask, self.tokenizer_tgt, max_len, self.device)
+            model_out = self.greedy_decode(self.model, encoder_input, encoder_mask, max_len)
 
             source_text = dp["src_text"][0]
             # target_text = dp["tgt_text"][0]
@@ -97,7 +96,7 @@ class Inferer:
             # expected.append(target_text)
             predicted.append(model_out_text)
 
-            model_out = self.greedy_decode(self.model, encoder_input, encoder_mask, self.tokenizer_tgt, max_len, self.device)
+            model_out = self.greedy_decode(self.model, encoder_input, encoder_mask, max_len)
 
             model_out_text = self.tokenizer_tgt.decode(model_out.detach().cpu().numpy())
 
@@ -113,12 +112,11 @@ class Inferer:
                 output_file.write(f"{model_out_text}\n")
 
     def load_model(self, path):
-        self.model = get_model(self.config, self.tokenizer_src.get_vocab_size(), self.tokenizer_tgt.get_vocab_size()).to(
+        self.model = get_model(self.config, self.tokenizer_src.get_vocab_size(),
+                               self.tokenizer_tgt.get_vocab_size()).to(
             self.device)
         self.model.load_state_dict(torch.load(path)['model_state_dict'])
         return self.model
-
-
 
 
 if __name__ == '__main__':
