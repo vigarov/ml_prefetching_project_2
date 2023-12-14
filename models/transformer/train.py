31cc75fb5da798d0e9c30727857c3a1663aa2c4d
Thibault Czarniak <thibault.czarniak@epfl.ch>
2023-12-14 15:31:17 +0100

    Implemented beam_search_decoder

--- models/transformer/train.py
+++ models/transformer/train.py
@@ -17,11 +17,12 @@ from pathlib import Path
 from datasets import load_dataset
 from tokenizers import Tokenizer
 from tokenizers.models import BPE
-from tokenizers.trainers import  BpeTrainer
+from tokenizers.trainers import BpeTrainer
 from tokenizers.pre_tokenizers import Whitespace
 
 import torchmetrics
 from torch.utils.tensorboard import SummaryWriter
+
 
 def greedy_decode(model, source, source_mask, tokenizer_src, tokenizer_tgt, max_len, device):
     sos_idx = tokenizer_tgt.token_to_id('[SOS]')
@@ -54,7 +55,65 @@ def greedy_decode(model, source, source_mask, tokenizer_src, tokenizer_tgt, max_
     return decoder_input.squeeze(0)
 
 
-def run_validation(model, validation_ds, tokenizer_src, tokenizer_tgt, max_len, device, print_msg, global_step, writer, num_examples=2):
+def beam_search_decode(model, beam_size, source, source_mask, tokenizer_src, tokenizer_tgt, max_len, device):
+    sos_idx = tokenizer_tgt.token_to_id('[SOS]')
+    eos_idx = tokenizer_tgt.token_to_id('[EOS]')
+
+    # Precompute the encoder output and reuse it for every step
+    encoder_output = model.encode(source, source_mask)
+    # Initialize the decoder input with the sos token
+    decoder_initial_input = torch.empty(1, 1).fill_(sos_idx).type_as(source).to(device)
+
+    # Create a candidate list
+    candidates = [(decoder_initial_input, 1)]
+
+    while True:
+
+        # If a candidate has reached the maximum length, it means we have run the decoding for at least max_len iterations, so stop the search
+        if any([cand.size(1) == max_len for cand, _ in candidates]):
+            break
+
+        # Create a new list of candidates
+        new_candidates = []
+
+        for candidate, score in candidates:
+
+            # Do not expand candidates that have reached the eos token
+            if candidate[0][-1].item() == eos_idx:
+                continue
+
+            # Build the candidate's mask
+            candidate_mask = causal_mask(candidate.size(1)).type_as(source_mask).to(device)
+            # calculate output
+            out = model.decode(encoder_output, source_mask, candidate, candidate_mask)
+            # get next token probabilities
+            prob = model.project(out[:, -1])
+            # get the top k candidates
+            topk_prob, topk_idx = torch.topk(prob, beam_size, dim=1)
+            for i in range(beam_size):
+                # for each of the top k candidates, get the token and its probability
+                token = topk_idx[0][i].unsqueeze(0).unsqueeze(0)
+                token_prob = topk_prob[0][i].item()
+                # create a new candidate by appending the token to the current candidate
+                new_candidate = torch.cat([candidate, token], dim=1)
+                # We sum the log probabilities because the probabilities are in log space
+                new_candidates.append((new_candidate, score + token_prob))
+
+        # Sort the new candidates by their score
+        candidates = sorted(new_candidates, key=lambda x: x[1], reverse=True)
+        # Keep only the top k candidates
+        candidates = candidates[:beam_size]
+
+        # If all the candidates have reached the eos token, stop
+        if all([cand[0][-1].item() == eos_idx for cand, _ in candidates]):
+            break
+
+    # Return the best candidate
+    return candidates[0][0].squeeze()
+
+
+def run_validation(model, validation_ds, tokenizer_src, tokenizer_tgt, max_len, device, print_msg, global_step, writer,
+                   num_examples=2, algo_decode="greedy"):
     model.eval()
     count = 0
 
@@ -74,14 +133,19 @@ def run_validation(model, validation_ds, tokenizer_src, tokenizer_tgt, max_len, 
     with torch.no_grad():
         for batch in validation_ds:
             count += 1
-            encoder_input = batch["encoder_input"].to(device) # (b, seq_len)
-            encoder_mask = batch["encoder_mask"].to(device) # (b, 1, 1, seq_len)
+            encoder_input = batch["encoder_input"].to(device)  # (b, seq_len)
+            encoder_mask = batch["encoder_mask"].to(device)  # (b, 1, 1, seq_len)
 
             # check that the batch size is 1
             assert encoder_input.size(
                 0) == 1, "Batch size must be 1 for validation"
 
-            model_out = greedy_decode(model, encoder_input, encoder_mask, tokenizer_src, tokenizer_tgt, max_len, device)
+            if algo_decode == "beam":
+                model_out = beam_search_decode(model, 5, encoder_input, encoder_mask, tokenizer_src, tokenizer_tgt,
+                                               max_len)
+            else:
+                model_out = greedy_decode(model, encoder_input, encoder_mask, tokenizer_src, tokenizer_tgt, max_len,
+                                          device)
 
             source_text = batch["src_text"][0]
             target_text = batch["tgt_text"][0]
@@ -90,17 +154,17 @@ def run_validation(model, validation_ds, tokenizer_src, tokenizer_tgt, max_len, 
             source_texts.append(source_text)
             expected.append(target_text)
             predicted.append(model_out_text)
-            
+
             # Print the source, target and model output
-            print_msg('-'*console_width)
+            print_msg('-' * console_width)
             print_msg(f"{f'SOURCE: ':>12}{source_text}")
             print_msg(f"{f'TARGET: ':>12}{target_text}")
             print_msg(f"{f'PREDICT:  ':>12}{model_out_text}")
 
             if count == num_examples:
-                print_msg('-'*console_width)
+                print_msg('-' * console_width)
                 break
-    
+
     if writer:
         # Evaluate the character error rate
         # Compute the char error rate 
@@ -124,16 +188,18 @@ def run_validation(model, validation_ds, tokenizer_src, tokenizer_tgt, max_len, 
         # Compute the BLEU metric
         new_pred = [set(a.split(" ")) for a in predicted]
         new_expected = [set(a.split(" ")) for a in expected]
-        acc = [len(a.intersection(b))/len(a) for a,b in zip(new_pred, new_expected)]
+        acc = [len(a.intersection(b)) / len(a) for a, b in zip(new_pred, new_expected)]
         acc = sum(acc) / len(acc)
         writer.add_scalar('validation accuracy', acc, global_step)
         writer.flush()
+
 
 def get_all_sentences(ds):
     for item in ds:
         yield item[config['lang_src']]
 
-def get_or_build_tokenizer(config, ds,feature_types):
+
+def get_or_build_tokenizer(config, ds, feature_types):
     for feature in feature_types:
         tokenizer_path = Path(config['tokenizer_file'].format())
         if not Path.exists(tokenizer_path):
@@ -147,12 +213,14 @@ def get_or_build_tokenizer(config, ds,feature_types):
             tokenizer = Tokenizer.from_file(str(tokenizer_path))
     return tokenizer
 
+
 def get_ds(config):
     # It only has the train split, so we divide it overselves
-    ds_raw = label(load_pandas(config["data_path"]), config["past_window"], config["k_predictions"]) #load_dataset(f"{config['datasource']}", f"{config['lang_src']}-{config['lang_tgt']}", split='train')
+    ds_raw = label(load_pandas(config["data_path"]), config["past_window"], config[
+        "k_predictions"])  # load_dataset(f"{config['datasource']}", f"{config['lang_src']}-{config['lang_tgt']}", split='train')
     print("loaded data")
     # Build tokenizers
-    tokenizer_src = get_or_build_tokenizer(config, ds_raw,config["input_features"])
+    tokenizer_src = get_or_build_tokenizer(config, ds_raw, config["input_features"])
     tokenizer_tgt = get_or_build_tokenizer(config, ds_raw)
 
     # Keep 90% for training, 10% for validation
@@ -160,8 +228,10 @@ def get_ds(config):
     val_ds_size = len(ds_raw) - train_ds_size
     train_ds_raw, val_ds_raw = random_split(ds_raw, [train_ds_size, val_ds_size])
 
-    train_ds = BilingualDataset(train_ds_raw, tokenizer_src, tokenizer_tgt, config['lang_src'], config['lang_tgt'], config['seq_len'])
-    val_ds = BilingualDataset(val_ds_raw, tokenizer_src, tokenizer_tgt, config['lang_src'], config['lang_tgt'], config['seq_len'])
+    train_ds = BilingualDataset(train_ds_raw, tokenizer_src, tokenizer_tgt, config['lang_src'], config['lang_tgt'],
+                                config['seq_len'])
+    val_ds = BilingualDataset(val_ds_raw, tokenizer_src, tokenizer_tgt, config['lang_src'], config['lang_tgt'],
+                              config['seq_len'])
 
     # Find the maximum length of each sentence in the source and target sentence
     max_len_src = 0
@@ -175,18 +245,19 @@ def get_ds(config):
 
     print(f'Max length of source sentence: {max_len_src}')
     print(f'Max length of target sentence: {max_len_tgt}')
-    
 
     train_dataloader = DataLoader(train_ds, batch_size=config['batch_size'], shuffle=True)
     val_dataloader = DataLoader(val_ds, batch_size=1, shuffle=True)
 
     return train_dataloader, val_dataloader, tokenizer_src, tokenizer_tgt
 
+
 def get_model(config, vocab_src_len, vocab_tgt_len):
     model = build_model(config, vocab_src_len, vocab_tgt_len, config["seq_len"], config['seq_len'])
     return model
 
-def train_model(config, complete_save=False):
+
+def train_model(config, algo_decode="greedy", complete_save=False):
     # Define the device
     device = get_device()
 
@@ -204,7 +275,8 @@ def train_model(config, complete_save=False):
     initial_epoch = 0
     global_step = 0
     preload = config['preload']
-    model_filename = latest_weights_file_path(config) if preload == 'latest' else get_weights_file_path(config, preload) if preload else None
+    model_filename = latest_weights_file_path(config) if preload == 'latest' else get_weights_file_path(config,
+                                                                                                        preload) if preload else None
     if model_filename:
         print(f'Preloading model {model_filename}')
         state = torch.load(model_filename)
@@ -222,19 +294,19 @@ def train_model(config, complete_save=False):
         model.train()
         batch_iterator = tqdm(train_dataloader, desc=f"Processing Epoch {epoch:02d}")
         for batch in batch_iterator:
-
-            encoder_input = batch['encoder_input'].to(device) # (b, seq_len)
-            decoder_input = batch['decoder_input'].to(device) # (B, seq_len)
-            encoder_mask = batch['encoder_mask'].to(device) # (B, 1, 1, seq_len)
-            decoder_mask = batch['decoder_mask'].to(device) # (B, 1, seq_len, seq_len)
+            encoder_input = batch['encoder_input'].to(device)  # (b, seq_len)
+            decoder_input = batch['decoder_input'].to(device)  # (B, seq_len)
+            encoder_mask = batch['encoder_mask'].to(device)  # (B, 1, 1, seq_len)
+            decoder_mask = batch['decoder_mask'].to(device)  # (B, 1, seq_len, seq_len)
 
             # Run the tensors through the encoder, decoder and the projection layer
-            encoder_output = model.encode(encoder_input, encoder_mask) # (B, seq_len, d_model)
-            decoder_output = model.decode(encoder_output, encoder_mask, decoder_input, decoder_mask) # (B, seq_len, d_model)
-            proj_output = model.project(decoder_output) # (B, seq_len, vocab_size)
+            encoder_output = model.encode(encoder_input, encoder_mask)  # (B, seq_len, d_model)
+            decoder_output = model.decode(encoder_output, encoder_mask, decoder_input,
+                                          decoder_mask)  # (B, seq_len, d_model)
+            proj_output = model.project(decoder_output)  # (B, seq_len, vocab_size)
 
             # Compare the output with the label
-            label = batch['label'].to(device) # (B, seq_len)
+            label = batch['label'].to(device)  # (B, seq_len)
 
             # Compute the loss using a simple cross entropy
             loss = loss_fn(proj_output.view(-1, tokenizer_tgt.get_vocab_size()), label.view(-1))
@@ -254,7 +326,8 @@ def train_model(config, complete_save=False):
             global_step += 1
 
         # Run validation at the end of every epoch
-        run_validation(model, val_dataloader, tokenizer_src, tokenizer_tgt, config['seq_len'], device, lambda msg: batch_iterator.write(msg), global_step, writer)
+        run_validation(model, val_dataloader, tokenizer_src, tokenizer_tgt, config['seq_len'], device,
+                       lambda msg: batch_iterator.write(msg), global_step, writer)
 
         # Save the model at the end of every epoch
         model_filename = get_weights_file_path(config, f"{epoch:02d}")
@@ -265,10 +338,12 @@ def train_model(config, complete_save=False):
             'global_step': global_step
         }, model_filename)
 
+
 def multi_config_train():
     configs = get_all_configs()
     for c in configs:
         train_model(c)
+
 
 if __name__ == '__main__':
     warnings.filterwarnings("ignore")
