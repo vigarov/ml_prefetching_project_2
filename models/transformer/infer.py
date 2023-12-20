420414a6cad5897769d640ae986579843e826ae5
Thibault Czarniak <thibault.czarniak@epfl.ch>
2023-12-20 22:25:45 +0100

    leapppp

--- models/transformer/infer.py
+++ models/transformer/infer.py
@@ -5,65 +5,44 @@ import torch
 import torchmetrics
 
 from models.transformer.config import get_device, get_config, latest_weights_file_path
-from models.transformer.dataset import causal_mask, BilingualDataset
+from models.transformer.dataset import causal_mask, PageFaultDataset
 from tokenizers import Tokenizer
 
-from models.transformer.train import get_model
+from models.transformer.train import get_model, get_tokenizers, greedy_decode, beam_search_decode
+from transformer.trained_tokenizers.special_tokenizers import SimpleTokenIdList
 
 
 class Inferer:
+    """
+    Class used to infer the output of a model
+    """
     def __init__(self, config, model=None, device=None):
-        tokenizer_src_path = Path(config['tokenizer_files'].format(config['lang_src']))
-        tokenizer_tgt_path = Path(config['tokenizer_files'].format(config['lang_tgt']))
-
-        tokenizer_src = Tokenizer.from_file(str(tokenizer_src_path))
-        tokenizer_tgt = Tokenizer.from_file(str(tokenizer_tgt_path))
-        self.tokenizer_src = tokenizer_src
-        self.tokenizer_tgt = tokenizer_tgt
-        self.src_lang = config['lang_src']
-        self.tgt_lang = config['lang_tgt']
+        """
+        :param config: the configuration (see config.py)
+        :param model: the model to use for inference (if None, will use the configuration)
+        :param device: the device to use for inference (if None, will use the configuration)
+        """
+        src_tokenizer, tgt_tokenizer = get_tokenizers(config)
+        self.src_tokenizer = src_tokenizer
+        self.tgt_tokenizer = tgt_tokenizer
         self.config = config
         self.device = get_device() if device is None else device
         path_weights = latest_weights_file_path(config)
         self.model = self.load_model(path_weights) if model is None else model
-
-    def greedy_decode(self, model, source, source_mask, max_len):
-        sos_idx = self.tokenizer_tgt.token_to_id('[SOS]')
-        eos_idx = self.tokenizer_tgt.token_to_id('[EOS]')
-
-        # Precompute the encoder output and reuse it for every step
-        encoder_output = model.encode(source, source_mask)
-        # Initialize the decoder input with the sos token
-        decoder_input = torch.empty(1, 1).fill_(sos_idx).type_as(source).to(self.device)
-        while True:
-            if decoder_input.size(1) == max_len:
-                break
-
-            # build mask for target
-            decoder_mask = causal_mask(decoder_input.size(1)).type_as(source_mask).to(
-                self.device)
-
-            # calculate output
-            out = model.decode(encoder_output, source_mask, decoder_input, decoder_mask)
-
-            # get next token
-            prob = model.project(out[:, -1])
-            _, next_word = torch.max(prob, dim=1)
-            decoder_input = torch.cat(
-                [decoder_input, torch.empty(1, 1).type_as(source).fill_(next_word.item()).to(self.device)], dim=1
-            )
-
-            if next_word == eos_idx:
-                break
-
-        return decoder_input.squeeze(0)
+        self.decode_algorithm = config["decode_algorithm"]
+        self.beam_size = config["beam_size"] #FIXME not implemented yet
 
     def infer(self, data, max_len, output_file=None):
+        """
+        Infer the output of the model for the given data and print the result to stdout (and to a file if specified)
+        :param data: the data to infer from
+        :param max_len: the maximum length of the output
+        :param output_file: the file to write the output to (if None, will only print to stdout)
+        :return: None
+        """
         self.model.eval()
         source_texts = []
-        predicted = []
-        dataAdapted = {}
-        dataAdapted[0] = {self.src_lang: data, self.tgt_lang: ""}
+        inferred = []
 
         try:
             # get the console window width
@@ -75,46 +54,59 @@ class Inferer:
             console_width = 80
 
         with torch.no_grad():
-
-            dataset = BilingualDataset(dataAdapted, self.tokenizer_src, self.tokenizer_tgt, self.src_lang,
-                                       self.tgt_lang,
-                                       max_len)
+            config = get_config()
+            subsample_rate = config["subsample"]
+            indices_train = list(range(len(data)))
+            dataset = PageFaultDataset(config, data, indices_train,
+                                       self.tokenizer_src,
+                                       self.tokenizer_tgt,
+                                       sample_percentage=subsample_rate
+                                       )
 
             # assert(dataset.__len__() == 1)
 
             dp = dataset.__getitem__(0)
-            encoder_input = dp["encoder_input"].to(self.device)  # (b, seq_len)
-            encoder_mask = dp["encoder_mask"].to(self.device)  # (b, 1, 1, seq_len)
+            encoder_input_list = dp["encoder_input"]  # [(b, I)]
+            encoder_input_list = [t.to(self.device) for t in encoder_input_list]
+            encoder_mask = dp["encoder_mask"].to(self.device)  # (b, 1, 1, I)
 
-            model_out = self.greedy_decode(self.model, encoder_input, encoder_mask, max_len)
+            # check that the batch size is 1
+            assert encoder_input_list[0].size(0) == 1, "Batch size must be 1 for validation"
+
+            if self.decode_algorithm == "beam":
+                model_out = beam_search_decode(self.model, self.beam_size, encoder_input_list, encoder_mask,
+                                               self.tgt_tokenizer, config["start_stop_generating_tokens"],
+                                               max_len, self.device)
+            else:
+                assert self.decode_algorithm == "greedy"
+                model_out = greedy_decode(self.model, encoder_input_list, encoder_mask, self.tgt_tokenizer, config["start_stop_generating_tokens"],
+                                          max_len,
+                                          self.device)
 
             source_text = dp["src_text"][0]
-            # target_text = dp["tgt_text"][0]
-            model_out_text = self.tokenizer_tgt.decode(model_out.detach().cpu().numpy())
+            target_text = dp["tgt_text"][0]
+            model_out_text = self.tgt_tokenizer.decode(SimpleTokenIdList(model_out.detach().cpu().numpy().tolist()))
 
             source_texts.append(source_text)
-            # expected.append(target_text)
-            predicted.append(model_out_text)
-
-            model_out = self.greedy_decode(self.model, encoder_input, encoder_mask, max_len)
-
-            model_out_text = self.tokenizer_tgt.decode(model_out.detach().cpu().numpy())
-
-            source_texts.append(data)
-            predicted.append(model_out_text)
+            inferred.append(model_out_text)
 
             # Print the source, target and model output
+            source_string = f"{f'SOURCE: ':>12}{source_text}"
+            infer_string = f"{f'INFERRED: ':>12}{model_out_text}"
             print('-' * console_width)
-            print(f"{f'SOURCE: ':>12}{data}")
-            print(f"{f'PREDICT:  ':>12}{model_out_text}")
+            print(source_string)
+            print(infer_string)
+            print('-' * console_width)
 
             if output_file is not None:
-                output_file.write(f"{model_out_text}\n")
+                with open(output_file, "w") as f:
+                    f.write(f"{source_string}\n")
+                    f.write(f"{infer_string}\n")
+            return model_out_text
 
     def load_model(self, path):
-        self.model = get_model(self.config, self.tokenizer_src.get_vocab_size(),
-                               self.tokenizer_tgt.get_vocab_size()).to(
-            self.device)
+        self.model = get_model(self.config, self.src_tokenizer,
+                               self.tgt_tokenizer).to(self.device)
         self.model.load_state_dict(torch.load(path)['model_state_dict'])
         return self.model
 
---
+++ models/transformer/leap_prefetcher.py
@@ -0,0 +1,71 @@
+# This script runs the leap prefetcher on 
+# a trace. 
+
+from pandas import *
+
+from transformer.config import get_config
+from transformer.dataset import PageFaultDataset
+from transformer.infer import Inferer
+from transformer.train import get_tokenizers
+
+trace_file = read_csv("data/processed/processed_canneal_v1.4.csv")
+
+# Function to find majority element
+def findMajority(arr, n):
+    candidate = -1
+    votes = 0
+
+    # Finding majority candidate
+    for i in range (n):
+        if (votes == 0):
+            candidate = arr[i]
+            votes = 1
+        else:
+            if (arr[i] == candidate):
+                votes += 1
+            else:
+                votes -= 1
+    count = 0
+
+    # Checking if majority candidate occurs more than n/2
+    # times
+    for i in range (n):
+        if (arr[i] == candidate):
+            count += 1
+    
+    if (count > n // 2):
+        return candidate
+    else:
+        return -1
+
+
+count = 0
+predictedLeap = 0
+not_predictedLeap = 0
+predictedOurs = 0
+not_predictedOurs = 0
+config = get_config()
+indices_train = list(range(len(trace_file)))
+src_tokenizer, tgt_tokenizer = get_tokenizers(config)
+inferer = Inferer(config)
+while count + 32 < len(trace_file):
+    deltaLeap = trace_file['prev_faults'][count: count + 32].tolist()
+    candidateOurs = inferer.infer(trace_file[count]['prev_faults'], 32)
+    candidateLeap = findMajority(deltaLeap, 32)
+    deltaLeap = trace_file[count+32]['y']
+    trueValue = trace_file[count+32]['y']
+    if deltaLeap == candidateLeap:
+        predictedLeap += 1
+    else:
+        not_predictedLeap += 1
+
+    if candidateOurs == trueValue:
+        predictedOurs += 1
+    else:
+        not_predictedOurs += 1
+    count += 1
+
+print("Predicted by Leap: ", predictedLeap)
+print("Not predicted by Leap: ", not_predictedLeap)
+print("Predicted by Ours: ", predictedOurs)
+print("Not predicted by Ours: ", not_predictedOurs)
